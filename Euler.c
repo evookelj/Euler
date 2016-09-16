239b548fdc33e56daada3e11dfb4694cf9ae622c
eVookelj <evukelj@stuy.edu>
2016-09-16 15:34:57 -0400

    Work on prob2()

--- Euler.c
+++ Euler.c
@@ -11,6 +11,21 @@ int prob1() {
 	return s;
 }
 
+int fibN(int n) {
+	if (n==1 || n==2) { return n; }
+	return fibN(n-1) + fibN(n-2);
+}
+
+int prob2() {
+	int n=1;
+	int s=0;
+	while (1) {
+		int temp = fibN(n);
+		if (!(temp%2)) { s+=temp; }
+		if (temp>4000000) { return s; }
+	}
+}
+
 int main() {
-	printf("%d\n", prob1());
+	printf("%d\n%d\n", prob1(), prob2());
 }
